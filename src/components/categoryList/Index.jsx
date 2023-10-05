42fde2d2014c5286ca6193044f1fc7fd9ef927c4
Shashi <shashikumarsharma59@gmail.com>
2023-10-05 19:28:43 +0530

    add more function

--- src/components/categoryList/Index.jsx
+++ src/components/categoryList/Index.jsx
@@ -2,20 +2,9 @@ import "../../styles/categorylist.css"
 import propTypes from "prop-types"
 import TodoButton from "../button"
 import Button from "../../staticdata/buttonstaticdata/ButtonData"
-import { useState } from "react"
 
 const CategoryList = (props) => {
   const { className,handlecategorydata} = props
-
-  // const [categoryData, setCategoryData] = useState([])
-  // const handleCategoryData = (e) => {
-  //   if (categoryData.includes(e.target.value)) {
-  //     setCategoryData(categoryData.filter((ele) => ele !== e.target.value))
-  //   } else {
-  //     setCategoryData([...categoryData, e.target.value])
-  //   }
-  // }
-  // console.log(categoryData,"++++++")
 
 
   return (
@@ -29,6 +18,7 @@ const CategoryList = (props) => {
             className={"category__button"}
             onclick={handlecategorydata}
             classname={ele.classname}
+            color={ele.color}
           />
         )
       })}
